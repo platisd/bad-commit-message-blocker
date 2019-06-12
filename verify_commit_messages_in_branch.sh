04b82052097294f0ded5423689ce13678b110bda
Dimitris Platis <platisd@users.noreply.github.com>
2019-06-12 21:13:18 +0200

    Explain purpose of helper script

--- verify_commit_messages_in_branch.sh
+++ verify_commit_messages_in_branch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# A helper script to verify the messages of every commit in
+# the current branch when compared against `origin/master`.
+# If there is no difference then the latest commit will be used.
 set -eu
 
 commits_since_master=$(git rev-list HEAD ^origin/master)
