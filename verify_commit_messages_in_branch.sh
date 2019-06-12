80191e166287ca409cb35bcec5fb9feaca32f606
Dimitris Platis <platisd@users.noreply.github.com>
2019-06-12 21:18:55 +0200

    Make the script executable from different folder

--- verify_commit_messages_in_branch.sh
+++ verify_commit_messages_in_branch.sh
@@ -4,9 +4,10 @@
 # If there is no difference then the latest commit will be used.
 set -eu
 
-commits_since_master=$(git rev-list HEAD ^origin/master)
+script_dir=$(dirname "$0")
+commits_since_master=$(git -C $script_dir rev-list HEAD ^origin/master)
 
 while read -r commit_hash; do
-    commit_message="$(git log --format=%B -n 1 $commit_hash)"
-    python3 bad_commit_message_blocker.py --message "$commit_message"
+    commit_message="$(git -C $script_dir log --format=%B -n 1 $commit_hash)"
+    python3 $script_dir/bad_commit_message_blocker.py --message "$commit_message"
 done <<< "$commits_since_master"
