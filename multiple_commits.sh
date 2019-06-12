bc20cb58ed344e9a6e5738c22af6ed6883c70efb
Dimitris Platis <platisd@users.noreply.github.com>
2019-06-12 20:22:09 +0200

    Check all messages in the branch
    Check the messages of all the commits in the branch
    we are about to merge to master.

---
+++ multiple_commits.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -eu
+
+commits_since_master=$(git rev-list HEAD ^master)
+
+while read -r commit_hash; do
+    commit_message="$(git log --format=%B -n 1 $commit_hash)"
+    python3 bad_commit_message_blocker.py --message "$commit_message"
+done <<< "$commits_since_master"
