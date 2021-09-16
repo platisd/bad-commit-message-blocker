da779be3837d14bf21219bc4fd83170235948968
Dimitris Platis <platisd@users.noreply.github.com>
2021-09-17 00:18:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 script_dir="$(dirname "$0")"
 cd $script_dir
 
-eval git clone "https://${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
+eval git clone "https://platisd:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
 cd $GITHUB_REPOSITORY
 eval git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 eval git fetch --all
