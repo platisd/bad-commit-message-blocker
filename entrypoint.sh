020647db1b6776da6e59d0f56d8565b6cc990735
platisd <platisd@users.noreply.github.com>
2021-09-17 00:48:24 +0200

    Fix clone not working for private repositories
    Seems like GITHUB_TOKEN doesn't work like it used to.
    Reported in #7.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ set -eu
 script_dir="$(dirname "$0")"
 cd $script_dir
 
-eval git clone "https://${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
+maintainer=${GITHUB_REPOSITORY%/*}
+eval git clone "https://${maintainer}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
 cd $GITHUB_REPOSITORY
 eval git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
 eval git fetch --all
